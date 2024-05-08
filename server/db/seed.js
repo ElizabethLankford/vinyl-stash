f9f436c16b41e230bbd3424894a0e1bb09e9b8bb
ElizabethLankford <elizabethlankford1@gmail.com>
2024-05-08 15:08:43 -0500

    set up seed file for database tables

---
+++ server/db/seed.js
@@ -0,0 +1,52 @@
+const client = require("./client");
+
+async function dropTables() {
+  try {
+    console.log("Dropping All Tables...");
+    await client.query(`
+        DROP TABLE IF EXISTS user;
+        DROP TABLE IF EXISTS vinyl;
+        DROP TABLE IF EXISTS household;
+        `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Building All Tables...");
+    await client.query(`
+        CREATE TABLE user (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(20) UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            name VARCHAR(20) NOT NULL,
+        );
+        CREATE TABLE vinyl (
+            id SERIAL PRIMARY KEY,
+            title TEXT NOT NULL,
+            artist VARCHAR(100) NOT NULL,
+            image TEXT,
+            genre VARCHAT(50),
+            user_id INTEGER REFERENCES user(id) NOT NULL,
+        );
+        `);
+    console.log("Tables successfully created!");
+  } catch (error) {
+    throw error;
+  }
+}
+
+const buildDb = async () => {
+  try {
+    client.connect();
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+};
+buildDb();
